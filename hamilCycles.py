572fc48b97b12834f6ed2bb01789b06bdbd6b280
robCampbell209 <roberthcampbell01@gmail.com>
2025-11-21 20:08:14 +0000

    implementing new hamiltonian cycle method that works and is consistent

---
+++ hamilCycles.py
@@ -0,0 +1,67 @@
+
+def indexToCoord(index, m, n):
+    # for an m x n grid
+    return (index % m, index // m)
+
+def coordToIndex(x, y, m, n):
+    return y * m + x
+
+def rotateCycle(cycle, m, n):
+    newCycle = []
+    for i in cycle:
+        x, y = indexToCoord(i, m, n)
+        newCycle.append(coordToIndex(y, x, n, m))
+    
+    return newCycle
+
+def hamilCycle(m, n):
+    rotate = False
+    if m % 2 == 1 and n % 2 == 1:
+        raise Exception("m or n must have a factor of 2")
+    elif m % 2 == 0 and n % 2 == 1:
+        rotate = True
+        m, n = n, m
+
+    cycle = [*range(m)]
+    print(cycle)
+    leftToRight = False
+    for row in range(1, n):
+        newRow = range(row * m + 1, (row + 1) * m)
+        if leftToRight == False:
+            newRow = reversed(newRow)
+
+        cycle.extend(newRow)
+        # print(list(newRow))
+        leftToRight = not leftToRight
+
+    # lastRow = []
+    for row in reversed(range(1, n)):
+        # lastRow.append(row * m)
+        cycle.append(row * m)
+    # print(lastRow)
+
+    if rotate == True:
+        return rotateCycle(cycle, m, n)
+    else:
+        return cycle
+
+def indexesAreNeighbours(i, j, m, n):
+    d = abs(i - j)
+    if d == 1 or d == m:
+        return True
+    else:
+        return False
+
+if __name__=="__main__":
+    m, n = 100, 100
+    c = hamilCycle(m, n)
+    print()
+    print(c)
+
+    for i in range(m * n):
+        assert i in c
+
+        assert indexesAreNeighbours(c[i - 1], c[i], m, n)
+    
+
+    
