cf52b5cc84f13048b54274c9140857bf79ce3591
robCampbell209 <roberthcampbell01@gmail.com>
2025-11-29 12:42:08 +0000

    stopped breaking on game completion

--- hamilSnake.py
+++ hamilSnake.py
@@ -14,22 +14,29 @@ def right(x, y):
 def nextPos(x, y):
     return [up(x, y), down(x, y), left(x, y), right(x, y)]
 
-class hamiltonianSnake:
-    def __init__(self, startPos, length, m, n):
-        self.head = [m//2, n//2]
+def inBounds(x, y, xMin, yMin, xMax, yMax):
+    if x >= xMin and x < xMax and y >= yMin and y < yMax:
+        return True
+    else:
+        return False
+
+class HamiltonianSnake:
+    def __init__(self, length, m, n):
+        self.m, self.n = m, n
+        self.head = (m//2, n//2)
         self.trail = []
         self.cycle = hamiltonianCycle(m, n)
-        self.cyclePos = self.cycle.index(self.snakePos[0])
+        self.cyclePos = self.cycle.index(self.head)
         for i in range(1, length):
             self.trail.append(self.cycle[self.cyclePos - i])
         self.randomiseApple()
 
     def randomiseApple(self):
-        possible = [p for p in self.cycle if p not in self.snakePos]
+        possible = [p for p in self.cycle if p not in self.position()]
         if len(possible) == 0:
-            self.applePos= (-1, -1)
+            self.apple= (-1, -1)
         else:
-            self.applePos = choice([p for p in self.cycle if p not in self.snakePos])
+            self.apple = choice(possible)
     
     def posInCycle(self, p):
         return self.cycle.index(p)
@@ -47,7 +54,7 @@ class hamiltonianSnake:
             return False
         
         if a == None:
-            a = self.posInCycle(self.applePos)
+            a = self.posInCycle(self.apple)
         if t == None:
             t = self.posInCycle(self.trail[-1])
         if h == None:
@@ -60,14 +67,22 @@ class hamiltonianSnake:
         return False
 
     def distToApple(self, p):
+        if self.apple == (-1, -1):
+            return 0
         possiblePos = self.cycle.index(p)
-        appleCyclePos = [*self.cycle[possiblePos:], *self.cycle].index(self.snakePos[0])
+        appleCyclePos = [*self.cycle[possiblePos:], *self.cycle].index(self.apple)
         return appleCyclePos - possiblePos
 
     def move(self):
-        newHead = min([p for p in nextPos(self.head) if self.isViable(p)], key=self.distToApple)
+        # newHead = min([p for p in nextPos(*self.head) if self.isViable(self.posInCycle(p))], key=self.distToApple)
+        newHead = min([p for p in nextPos(*self.head) if inBounds(*p, 0, 0, self.m, self.n) and self.isViable(self.posInCycle(p))], key=self.distToApple)
         self.trail = [self.head][:] + self.trail
         self.head = newHead
+        self.cyclePos = self.posInCycle(self.head)
+        if self.head != self.apple:
+            self.trail = self.trail[:-1]
+        else:
+            self.randomiseApple()
 
-    def snakeBody(self):
+    def position(self):
         return [self.head, *self.trail]
--- watchSnake.py
+++ watchSnake.py
@@ -1,1 +1,64 @@
 # Runs the hamiltonian snake with specified parameters
+import pygame
+from pygame.locals import *
+from hamilSnake import HamiltonianSnake
+from time import time
+
+squareSize = 50
+boardDim = (10, 10)
+moveTime = 0.0
+
+backgroundColour = (25, 25, 25)
+appleColour = (200, 200, 200)
+snakeColour = (0, 150, 0)
+# class Square(pygame.sprite.Sprite):
+#     def __init__(self, colour=(25, 25, 25)):
+#         super(Square, self).__init__()
+#         self.surf = pygame.Surface((squareSize - 2, squareSize - 2))
+#         self.surf.fill(colour)
+#         self.rect = self.surf.get_rect()
+
+pygame.init()
+screen = pygame.display.set_mode((boardDim[0] * squareSize, boardDim[1] * squareSize))
+screen.fill((50, 50, 50))
+
+# squares = [[Square() for i in range(boardDim[0])] for j in range(boardDim[1])]
+
+snake = HamiltonianSnake(3, *boardDim)
+
+lastMoveTime = time() - moveTime
+stop = False
+while stop != True:
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_BACKSPACE):
+            stop = True
+
+    if time() - lastMoveTime > moveTime:
+        lastMoveTime = time()
+        snake.move()
+
+        snakeBody = snake.position()
+        apple = snake.apple
+        screen.fill(backgroundColour)
+        for i in range(1, len(snakeBody)):
+            left = min(snakeBody[i], snakeBody[i - 1], key = lambda p : p[0])[0] * (squareSize) + 1
+            top = min(snakeBody[i], snakeBody[i - 1], key = lambda p : p[1])[1] * (squareSize) + 1
+            if snakeBody[i][0] == snakeBody[i - 1][0]:
+                width = (squareSize) - 2
+                height = (squareSize) * 2 - 2
+            elif snakeBody[i][1] == snakeBody[i - 1][1]:
+                width = (squareSize) * 2 - 2
+                height = (squareSize) - 2
+            else:
+                print("OH SHIT")
+
+            pygame.draw.rect(screen,
+                            snakeColour,
+                            pygame.Rect(left, top, width, height))
+        pygame.draw.rect(screen, appleColour, pygame.Rect(snake.apple[0] * squareSize + 1,
+                                                          snake.apple[1] * squareSize + 1,
+                                                          squareSize - 2, squareSize - 2))
+        
+        # if snake.isAlive() == False:
+        #     stop = True
+        pygame.display.flip()
