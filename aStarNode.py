73fc3faabdb53cc127c639d9cd8a46da6094700e
robCampbell209 <roberthcampbell01@gmail.com>
2025-12-20 14:12:43 +0000

    creating simple a star implementation

---
+++ aStarNode.py
@@ -0,0 +1,13 @@
+from math import inf
+
+class Node:
+    def __init__(self):
+        self.i = None
+        self.j = None
+        self.f = inf
+        self.g = inf
+        self.h = inf
+        self.parentI = None
+        self.parentJ = None
+
+
