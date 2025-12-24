654d6086bca40fa3a4ce70ecf8ebe581df37274b
robCampbell209 <roberthcampbell01@gmail.com>
2025-12-24 13:58:17 +0000

    a star for snake with hamiltonian logic

--- aStarForSnake.py
+++ aStarForSnake.py
@@ -1,39 +1,14 @@
-from aStarNode import Node
+from math import inf
+from hamilCycles import randHamilCycleCoords
 
-def printGrid(grid, startPos = (-1, -1), endPos = (-1, -1)):
-    output = " ■" * (len(grid[0]) + 2) + "\n ■"
-    for j in range(len(grid)):
-        for i in range(len(grid[j])):
-            output += " "
-            if (i, j) == startPos:
-                output += "0"
-            elif (i, j) == endPos:
-                output += "X"
-            elif grid[j][i] == 0:
-                output += "."
-            elif grid[j][i] == 1:
-                output += "■"
-        output += " ■\n ■"
-    output +=  " ■" * (len(grid[0]) + 1)
-    print(output)
-
-def printGridWithPath(grid, path):
-    output = " ■" * (len(grid[0]) + 2) + "\n ■"
-    for j in range(len(grid)):
-        for i in range(len(grid[j])):
-            output += " "
-            if (i, j) in path[1:-1]:
-                output += "o"
-            elif (i, j) == path[0] and (i, j) == path[-1]:
-                output += "X"
-            elif grid[j][i] == 0:
-                output += "."
-            elif grid[j][i] == 1:
-                output += "■"
-        output += " ■\n ■"
-    output +=  " ■" * (len(grid[0]) + 1)
-    print(output)
-
+class Node:
+    def __init__(self):
+        self.body = []
+        self.f = inf
+        self.g = inf
+        self.h = inf
+        self.parentI = None
+        self.parentJ = None
 
 def hamDist(pos, dest):
     return abs(pos[0] - dest[0]) + abs(pos[1] - dest[1])
@@ -61,6 +36,23 @@ def isValid(i, j, grid):
         return False
     else:
         return True
+
+def isAllowed(i, j, body, cycle):
+    m = max([p[0] for p in cycle])
+    n = max([p[1] for p in cycle])
+
+    s = cycle.index((i, j))
+    h = cycle.index(body[0])
+    t = cycle.index(body[-1])
+
+    a = (a - t) % (m * n)
+    h = (h - t) % (m * n)
+    s = (s - t) % (m * n)
+    t = 0
+    if s < h or s > a:
+        return False
+    else:
+        return True
     
 def tracePath(nodes, src, dest):
     path = []
@@ -71,25 +63,26 @@ def tracePath(nodes, src, dest):
 
     return [src] + path
 
-def aStarSearch(src, dest, grid):
-    if src == dest:
+def aStarSearch(snakePosition, apple, grid, cycle):
+    if snakePosition[0] == apple:
         raise Exception("source and destination are the same")
-    elif isValid(*src, grid) == False:
+    elif isValid(*snakePosition[0], grid) == False:
         raise Exception("invalid source")
-    elif isValid(*dest, grid) == False:
+    elif isValid(*apple, grid) == False:
         raise Exception("invalid source")
     
     visited = [[False for i in range(len(grid[j]))] for j in range(len(grid))]
     nodes = [[Node() for i in range(len(grid[j]))] for j in range(len(grid))]
 
-    i, j = src[:]
+    i, j = snakePosition[0]
+    nodes[j][i].body = snakePosition[:]
     nodes[j][i].f = 0
     nodes[j][i].g = 0
     nodes[j][i].h = 0
     # nodes[j][i].parentI = 0
     # nodes[j][i].parentJ = 0
 
-    heap = [(0, i, j)]
+    heap = [(0, snakePosition)]
 
     found = False
     while len(heap) > 0:
@@ -98,19 +91,21 @@ def aStarSearch(src, dest, grid):
         visited[j][i] = True
         nbours = neighbours((i, j))
         for i2, j2 in nbours:
-            if isValid(i2, j2, grid) and visited[j2][i2] == False:
-                if (i2, j2) == dest:
+            newBody = [(i2, j2)] + nodes[i][j].body[:-1]
+            if isValid(i2, j2, grid) and visited[j2][i2] == False and isAllowed(i2, j2, newBody, cycle):
+                if (i2, j2) == apple:
                     nodes[j2][i2].parentI = i
                     nodes[j2][i2].parentJ = j
                     found = True
-                    return tracePath(nodes, src, dest)
+                    return tracePath(nodes, snakePosition[0], apple)
                 else:
                     g2 = nodes[j][i].g + 1
-                    h2 = hamDist((i2, j2), dest)
+                    h2 = hamDist((i2, j2), apple)
                     f2 = g2 + h2
                     if nodes[j2][i2].f > f2:
                         heap = insert((f2, i2, j2), heap, key=lambda x : x[0])
                         # print(len(heap))
+                        nodes.body = newBody[:]
                         nodes[j2][i2].f = f2
                         nodes[j2][i2].g = g2
                         nodes[j2][i2].h = h2
@@ -119,28 +114,3 @@ def aStarSearch(src, dest, grid):
     if found == False:
         # raise Exception("No path between source and destiniation")
         return []
-
-
-grid = [
-    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1],
-    [0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
-    [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-]
-src = (0, 0)
-dest = (1, 2)
-
-# grid = [
-#     [0, 1, 0, 0],
-#     [0, 1, 0, 0],
-#     [0, 1, 0, 0],
-#     [0, 0, 0, 0],
-# ]
-# src = (0, 0)
-# dest = (3, 3)
-path = aStarSearch(src, dest, grid)
-printGridWithPath(grid, path)
